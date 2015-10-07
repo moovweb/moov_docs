5cab5da43f230f8eac64a4e8266ac509a191b0ba
Chris Neale <cin210@nyu.edu>
2015-10-06 17:39:39 -0700

    Invoke npm_jsdoc on all moov_ modules
    We want to generate documents inside the docs folder of this repo, not the out folder inside all our other modules.
    
    This should just look at all moov_ modules, compile their jsdocs using the template they provide, and save them to ./docs/node_modules/.

--- util/npm_jsdoc.sh
+++ util/npm_jsdoc.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-for dir in ./node_modules/*/
+for dir in ./node_modules/*
 do
-    (cd $dir && npm run jsdoc)
+    if [[ $dir == *"moov_"* ]]
+    then
+      echo "running jsdoc on $dir"
+      ./node_modules/.bin/jsdoc $dir -t ./node_modules/latodoc -c $dir/util/jsdoc_config.json -d ./docs/$dir $dir/lib $dir/README.md
+    else
+      echo "skipping $dir"
+    fi
 done
