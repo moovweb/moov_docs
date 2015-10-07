314bf67d871ab7c493f6756cedd27253479dc900
Chris Neale <cin210@nyu.edu>
2015-10-07 14:55:08 -0700

    Updating the template, regenerating
    Remove nesting, use one config file for jsdoc
    
    Add tmpl and use relative path
    
    Inject our our accordian layout into docs structure
    
    Remove Home link for now
    
    Remove h4 for members
    
    Modify template
    
    We don\'t want static assets, we just want content. Also, get rid of html in the links.
    
    Cleanup bad paths
    
    Sanitization intensifies
    
    Smarter link overwriting
    
    Catch the index and global links
    
    Don't clobber the ancestors
    
    Remove inline linenums script
    
    Update docs

--- util/npm_jsdoc.sh
+++ util/npm_jsdoc.sh
@@ -5,7 +5,8 @@ do
     if [[ $dir == *"moov_"* ]]
     then
       echo "running jsdoc on $dir"
-      ./node_modules/.bin/jsdoc $dir -t ./node_modules/latodoc -c $dir/util/jsdoc_config.json -d ./docs/$dir $dir/lib $dir/README.md
+      BASENAME=$(basename "$dir")
+      ./node_modules/.bin/jsdoc $dir -c ./util/jsdoc_config.json -d ./docs/$BASENAME $dir/lib $dir/README.md
     else
       echo "skipping $dir"
     fi
